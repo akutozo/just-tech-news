bed129850ccd3951f13665f4d9ca50a1d2f2efff
Jacob Daughtry <akutozo@gmail.com>
2020-09-03 12:07:41 -0500

    14.2 end

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
+const path = require('path');
 const express = require('express');
+const exphbs = require('express-handlebars');
+const session = require('express-session');
+
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
-const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: process.env.Secret,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
 //Handlebars
-const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
 
 app.engine('handlebars', hbs.engine);
